d181a54e0175b69b88868d98811fdcd12b844322
marco4512 <marco_alex45@hotmail.com>
2023-03-13 18:54:34 -0600

    Centrando el login

--- ptc_web/src/components/Login.jsx
+++ ptc_web/src/components/Login.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-function Login(){
-    return(
+function Login() {
+    return (
         <>
-                <div className="container" style={{background:"lightgray", marginTop:20, padding:20}}>
-        
-        <form id="form_login">
-            <div>
-                <h1 style={{color:"blue", textalign:"center"}}>LOGIN</h1>
-                <label htmlFor="txtusu"><strong>Username</strong></label>
-                <input type="text" id="txtusu" style={{textAlign:"center"}} className="form-control"  required/>
+        <br />
+            <div className="container" >
+                <form id="form_login">
+                    <div className="parteSuperior">
+                        <h1>LOGIN</h1>
+                        <label htmlFor="txtusu"><strong>Username</strong></label>
+                        <input type="text" id="txtusu"  className="form-control" required />
+                    </div>
+                    <div>
+                        <label htmlFor="txtpas"><strong>Password</strong></label>
+                        <input type="password" id="txtpas"  className="form-control" required />
+                    </div><br />
+                    <input type="submit" className="btn btn-primary" value="Login" />
+                </form>
             </div>
-            <div>
-                <label htmlFor="txtpas"><strong>Password</strong></label>
-                <input type="password" id="txtpas" style={{textAlign:"center"}} className="form-control"  required/>
-            </div><br/>
-            <input type="submit"  className="btn btn-primary" value="Login"/>
-        </form>
-
-    </div>
-    
-            
         </>
-        
+
     );
 }
 export default Login;
