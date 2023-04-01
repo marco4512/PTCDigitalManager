cabba00b55fa031945b210bb56233ecf15c7f06f
Alejandro <alejandrovazquez3069@gmail.com>
2023-03-31 21:28:36 -0600

    Panel Principal Estructura

--- ptc_web/src/components/PanelPrincipal.jsx
+++ ptc_web/src/components/PanelPrincipal.jsx
@@ -57,18 +57,18 @@ function PanelPrincipal(props) {
         querySnapshot.forEach((doc) => {
             switch (doc.data().rol) {
                 case 'Admin':
-                    inventarioNav.style.color="white";
-                    Pedido.style.color="white";
-                    productosNav.style.color="white";
-                    reportesNav.style.color="white";
-                    panelPrincipalNav.style.color="white";
+                    inventarioNav.style.color = "white";
+                    Pedido.style.color = "white";
+                    productosNav.style.color = "white";
+                    reportesNav.style.color = "white";
+                    panelPrincipalNav.style.color = "white";
                     break
                 case 'Asistente':
-                    inventarioNav.style.color="white";
-                    Pedido.style.color="white";
-                    productosNav.style.color="white";
-                    reportesNav.style.display="none";
-                    panelPrincipalNav.style.display="none";
+                    inventarioNav.style.color = "white";
+                    Pedido.style.color = "white";
+                    productosNav.style.color = "white";
+                    reportesNav.style.display = "none";
+                    panelPrincipalNav.style.display = "none";
                     break
 
             }
@@ -86,6 +86,61 @@ function PanelPrincipal(props) {
                     <button onClick={PanelPrincipal} id="panelPrincipalNav" className="buttonOpcion2">PanelPrincipal</button>
                 </div>
                 <h1 className="home">PanelPrincipal</h1>
+                <h1 className="text-center mx-auto">Nombre de Usuario</h1>
+                <br /><br /><br />
+
+
+                <div className="row text-center pb-5">
+                    <div className="col-10">
+                        <div>
+                            <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio1">Hoy</label>
+
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio2">Ayer</label>
+
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio3">Semana</label>
+
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio4">Fecha</label>
+                            </div>
+                            <div className="btn-group mx-3" role="group" aria-label="Basic radio toggle button group">
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio5" autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio5">Todos</label>
+
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio6" autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio6">Cliente</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div className="row pb-5 px-5">
+                    <div className="panel-lg text-center col-6 bg-dark mx-3">Pedidos</div>
+                    <div id="mi-col" className="col-5">
+                        <div class="child bg-dark">Pendientes</div>
+                        <div class="child bg-dark">Proceso</div>
+                        <div class="child bg-dark">Realizados</div>
+                    </div>
+                </div>
+
+                <div className="row pb-5 px-5">
+                    <div id="mi-col" className="col-5">
+                        <div class="child bg-dark">En Stock</div>
+                        <div class="child bg-dark">Comprometidas</div>
+                        <div class="child bg-dark">Necesitadas</div>
+                    </div>
+                    <div className="panel-lg col-6 bg-dark mx-3">Existencias</div>
+                </div>
+
+                <div className="row h-70 pb-5 px-5">
+                    <div className="panel-lg col-5 bg-dark mx-4">Ventas</div>
+                    <div className="panel-lg col-5 bg-dark mx-4">Clientes</div>
+                </div>
             </div>
         </>
     );
